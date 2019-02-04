13ca3aa10100c6a75d5d0243653f457f67e89f55
romain phet <romain.phet@gmail.com>
2019-02-04 12:02:22 +0100

    Resolution de conflits - routes

---
+++ front/src/pages/CreateAnnotationForm.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import "antd/dist/antd.css";
+import { Form, Icon, Input, Button } from "antd";
+
+class CreateAnnotationForm extends Component {
+  public render() {
+    return <Button type="primary">Button</Button>;
+  }
+}
+
+export default CreateAnnotationForm;
