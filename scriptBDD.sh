38a73317bff5ad1244abe9b6eabceedf4f06521e
rolextaing <rolex.taing@gmail.com>
2019-02-06 17:03:07 +0100

    Ajout script BDD, work in progress init.sql

---
+++ scriptBDD.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo rm -fr /db/datas
+sudo docker rm $(docker ps -a -q)
+sudo docker rmi $(docker images -q)
+sudo docker system prune --volumes
+sudo docker system prune
+sudo docker-compose up -V database
