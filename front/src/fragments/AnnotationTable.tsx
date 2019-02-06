d29f2db7ff72d009f177981d2f8ac08a4d6cc09b
Alex PLIEZ <alex.pliez@gmail.com>
2019-02-06 14:40:24 +0100

    Added fully fonctional tables and fixed CSS icons

--- front/src/fragments/AnnotationTable.tsx
+++ front/src/fragments/AnnotationTable.tsx
@@ -3,6 +3,7 @@ import { Table, Input, Button, Icon } from 'antd';
 import 'antd/dist/antd.css';
 import { number, string } from 'prop-types';
 import { ColumnProps } from 'antd/lib/table';
+import { TapType } from 'tapable';
 
 interface Annotation {
   signalId: number;
@@ -41,138 +42,183 @@ const data: Annotation[] = [
   }
 ];
 
-const columns: Array<ColumnProps<Annotation>> = [
-  {
-    title: 'Signal ID',
-    dataIndex: 'signalId',
-    sorter: (a: Annotation, b: Annotation) => a.signalId - b.signalId
-  },
-  {
-    title: 'Annotation',
-    dataIndex: 'annotation',
-    sorter: (a: Annotation, b: Annotation) =>
-      a.annotation.localeCompare(b.annotation, 'en', {
-        sensitivity: 'base',
-        ignorePunctuation: true
-      })
-  },
-  {
-    title: 'Creation date',
-    dataIndex: 'creationDate',
-    render: (date: Date) => date.toLocaleDateString('fr-FR'),
-    sorter: (a: Annotation, b: Annotation) =>
-      a.creationDate.getTime() - b.creationDate.getTime()
-  },
-  {
-    title: 'Last edit',
-    dataIndex: 'lastEdit',
-    render: (date: Date | undefined) => {
-      if (date === undefined) {
-        return '-';
-      }
-      return date.toLocaleDateString('fr-FR');
-    },
-    sorter: (a: Annotation, b: Annotation) => {
-      let timeA = 0;
-      let timeB = 0;
-      if (a.lastEdit !== undefined) {
-        timeA = a.lastEdit.getTime();
-      }
-      if (b.lastEdit !== undefined) {
-        timeB = b.lastEdit.getTime();
-      }
-      return timeA - timeB;
-    }
-  },
-  {
-    title: 'Status',
-    dataIndex: 'status',
-    filters: data
-      .map(a => a.status)
-      .filter((s, i, array) => array.indexOf(s) === i)
-      .map(s => ({ text: s, value: s })),
-    onFilter: (value: string, record: Annotation) =>
-      record.status.indexOf(value) === 0,
-    sorter: (a: Annotation, b: Annotation) =>
-      a.status.localeCompare(b.status, 'en', {
-        sensitivity: 'base',
-        ignorePunctuation: true
-      })
-  }
-];
 class AnnotationTable extends Component {
   public state = {
-    searchText: ''
+    searches: new Map(),
+    currentData: data.slice()
   };
 
-  /* public getColumnSearchProps = (dataIndex: number) => ({
-    filterDropdown: ({
-      setSelectedKeys,
-      selectedKeys,
-      confirm,
-      clearFilters
-    }) => (
-      <div style={{ padding: 8 }}>
-        <Input
-          ref={node => {
-            'this.searchInput = node';
-          }}
-          placeholder={`Search ${dataIndex}`}
-          value={selectedKeys[0]}
-          onChange={e =>
-            setSelectedKeys(e.target.value ? [e.target.value] : [])
-          }
-          onPressEnter={() => this.handleSearch(selectedKeys, confirm)}
-          style={{ width: 188, marginBottom: 8, display: 'block' }}
-        />
-        <Button
-          type='primary'
-          onClick={() => this.handleSearch(selectedKeys, confirm)}
-          icon='search'
-          size='small'
-          style={{ width: 90, marginRight: 8 }}
-        >
-          Search
-        </Button>
-        <Button
-          onClick={() => this.handleReset(clearFilters)}
-          size='small'
-          style={{ width: 90 }}
-        >
-          Reset
-        </Button>
-      </div>
-    ),
-    filterIcon: (filtered: boolean) => (
-      <Icon type='search' style={{ color: filtered ? '#1890ff' : undefined }} />
-    ),
-    onFilter: (value: string, record: string[]) =>
-      record[dataIndex]
-        .toString()
-        .toLowerCase()
-        .includes(value.toLowerCase()),
-    onFilterDropdownVisibleChange: (visible: boolean) => {
-      if (visible) {
-        setTimeout(() => 'this.searchInput.select()');
-      }
+  public columns: Array<ColumnProps<Annotation>> = [
+    {
+      title: () => this.getColumnSearchBox('signalId', 'signal ID'),
+      children: [
+        {
+          title: 'Signal ID',
+          dataIndex: 'signalId',
+          sorter: (a: Annotation, b: Annotation) => a.signalId - b.signalId
+        }
+      ]
     },
-    render: (text: string) => <mark>this.state.searchText</mark>
-  })
-*/
-  public handleSearch = (selectedKeys: string[], confirm: VoidFunction) => {
-    confirm();
-    this.setState({ searchText: selectedKeys[0] });
+    {
+      title: () => this.getColumnSearchBox('annotation', 'annotation'),
+      children: [
+        {
+          title: 'Annotation',
+          dataIndex: 'annotation',
+          sorter: (a: Annotation, b: Annotation) =>
+            a.annotation.localeCompare(b.annotation, 'en', {
+              sensitivity: 'base',
+              ignorePunctuation: true
+            })
+        }
+      ]
+    },
+    {
+      title: () => this.getColumnSearchBox('creationDate', 'creation date'),
+      children: [
+        {
+          title: 'Creation date',
+          dataIndex: 'creationDate',
+          render: (date: Date) => date.toLocaleDateString('fr-FR'),
+          sorter: (a: Annotation, b: Annotation) =>
+            a.creationDate.getTime() - b.creationDate.getTime()
+        }
+      ]
+    },
+    {
+      title: () => this.getColumnSearchBox('lastEdit', 'last edit date'),
+      children: [
+        {
+          title: 'Last edit',
+          dataIndex: 'lastEdit',
+          render: (date: Date | undefined) => {
+            if (date === undefined) {
+              return '-';
+            }
+            return date.toLocaleDateString('fr-FR');
+          },
+          sorter: (a: Annotation, b: Annotation) => {
+            let timeA = 0;
+            let timeB = 0;
+            if (a.lastEdit !== undefined) {
+              timeA = a.lastEdit.getTime();
+            }
+            if (b.lastEdit !== undefined) {
+              timeB = b.lastEdit.getTime();
+            }
+            return timeA - timeB;
+          }
+        }
+      ]
+    },
+    {
+      title: 'Status',
+      dataIndex: 'status',
+      filters: data
+        .map(a => a.status)
+        .filter((s, i, array) => array.indexOf(s) === i)
+        .map(s => ({ text: s, value: s })),
+      onFilter: (value: string, record: Annotation) =>
+        record.status.indexOf(value) === 0,
+      sorter: (a: Annotation, b: Annotation) =>
+        a.status.localeCompare(b.status, 'en', {
+          sensitivity: 'base',
+          ignorePunctuation: true
+        })
+    }
+  ];
+
+  public getColumnSearchBox = (
+    dataIndex: keyof Annotation,
+    displayText: string
+  ) => (
+    <div style={{ paddingTop: 8, textAlign: 'center' }}>
+      <Input
+        placeholder={`Search by ${displayText}`}
+        onChange={e => this.handleChange(dataIndex, e.target.value)}
+      />
+    </div>
+  )
+
+  public handleChange = (dataIndex: keyof Annotation, value: string) => {
+    this.state.searches.set(dataIndex, value);
+    this.handleSearch();
   }
 
-  public handleReset = (clearFilters: VoidFunction) => {
-    clearFilters();
-    this.setState({ searchText: '' });
+  public handleSearch = () => {
+    this.state.currentData = data.slice();
+    const filteredData = this.state.currentData.filter(record => {
+      if (this.state.searches.get('signalId')) {
+        if (
+          !record.signalId
+            .toString()
+            .startsWith(this.state.searches.get('signalId'))
+        ) {
+          return false;
+        }
+      }
+      if (this.state.searches.get('annotation')) {
+        if (
+          !record.annotation
+            .toString()
+            .includes(this.state.searches.get('annotation'))
+        ) {
+          return false;
+        }
+      }
+      if (record.creationDate && this.state.searches.get('creationDate')) {
+        if (
+          !record.creationDate
+            .toLocaleDateString('fr-FR')
+            .includes(this.state.searches.get('creationDate'))
+        ) {
+          return false;
+        }
+      }
+      if (this.state.searches.get('lastEdit')) {
+        if (!record.lastEdit && this.state.searches.get('lastEdit') !== '-') {
+          return false;
+        }
+        if (
+          record.lastEdit &&
+          !record.lastEdit
+            .toLocaleDateString('fr-FR')
+            .includes(this.state.searches.get('lastEdit'))
+        ) {
+          return false;
+        }
+      }
+      if (this.state.searches.get('status')) {
+        if (
+          !record.status
+            .toString()
+            .startsWith(this.state.searches.get('status'))
+        ) {
+          return false;
+        }
+      }
+      return true;
+    });
+
+    this.setState({
+      currentData: filteredData
+    });
   }
 
   public render() {
     return (
       <div>
-        <Table rowKey='signalId' columns={columns} dataSource={data} />,
+        <Table
+          rowKey='signalId'
+          columns={this.columns}
+          dataSource={this.state.currentData}
+          pagination={{
+            position: 'bottom',
+            pageSizeOptions: ['10', '20', '30', '40'],
+            showSizeChanger: true,
+            showTotal: (total, range) => `${range[0]}-${range[1]} of ${total} items`
+          }}
+        />
       </div>
     );
   }
--- front/src/fragments/Header.tsx
+++ front/src/fragments/Header.tsx
@@ -1,79 +1,79 @@
-import React, { MouseEvent, Component } from "react";
+import React, { MouseEvent, Component } from 'react';
 import {
   BrowserRouter as Router,
   Route,
   Link,
   LinkProps
-} from "react-router-dom";
-import { Row, Col, Menu, Icon, Alert } from "antd";
-import { ClickParam } from "antd/lib/menu";
+} from 'react-router-dom';
+import { Row, Col, Menu, Icon, Alert } from 'antd';
+import { ClickParam } from 'antd/lib/menu';
 
-import logo from "../assets/images/logo.png";
-import "../assets/styles/App.css";
+import logo from '../assets/images/logo.png';
+import '../assets/styles/App.css';
 
 const SubMenu = Menu.SubMenu;
 const MenuItemGroup = Menu.ItemGroup;
 
 // this is a class because it needs state
 class Header extends Component {
-  state = {
-    current: "Home"
+  public state = {
+    current: 'Home'
   };
 
-  handleClick = (e: ClickParam) => {
+  public handleClick = (e: ClickParam) => {
     this.setState({
       current: e.key
     });
-  };
+  }
 
-  handleClickHome = (e: MouseEvent<HTMLElement>) => {
+  public handleClickHome = (e: MouseEvent<HTMLElement>) => {
     this.setState({
-      current: "Home"
+      current: 'Home'
     });
-  };
+  }
 
-  render() {
+  public render() {
     return (
-      <div className="navbar-container">
-        <Link to="/" onClick={this.handleClickHome}>
-          <img src={logo} className="logo" alt="logo" />
+      <div className='navbar-container'>
+        <Link to='/' onClick={this.handleClickHome}>
+          <img src={logo} className='logo' alt='logo' />
         </Link>
-        <div className="menu-container">
-          <h1 className="page-title">{this.state.current}</h1>
+        <div className='menu-container'>
+          <h1 className='page-title'>{this.state.current}</h1>
           <Menu
             onClick={this.handleClick}
             selectedKeys={[this.state.current]}
-            mode="horizontal"
-            className="main-menu"
+            mode='horizontal'
+            className='main-menu'
           >
-            <Menu.Item key="Create User">
-              <Link to="/CreateUser/">
-                <span className="main-menu-item-text">
-                  <Icon type="user-add" />
+            <Menu.Item key='Create User'>
+              <Link to='/CreateUser/'>
+                <span className='main-menu-item-text'>
+                  <Icon type='user-add' className='anticon-title' />
                   Create User
                 </span>
               </Link>
             </Menu.Item>
-            <Menu.Item key="Create Tag">
-              <Link to="/CreateTag/">
-                <span className="main-menu-item-text">
-                  <Icon type="tag" />
+            <Menu.Item key='Create Tag'>
+              <Link to='/CreateTag/'>
+                <span className='main-menu-item-text'>
+                  <Icon type='tag' className='anticon-title' />
                   Create Tag
                 </span>
               </Link>
             </Menu.Item>
-            <Menu.Item key="Dashboard">
-              <Link to="/Dashboard/">
-                <span className="main-menu-item-text">
-                  <Icon type="dashboard" />
+            <Menu.Item key='Dashboard'>
+              <Link to='/Dashboard/'>
+                <span className='main-menu-item-text'>
+                  <Icon type='dashboard' className='anticon-title' />
                   Dashboard
                 </span>
               </Link>
             </Menu.Item>
-            <Menu.Item key="Notifications">
-              <Link to="/Notifications/">
-                <span className="main-menu-item-text">
-                  <Icon type="bell" />
+            <Menu.Item key='Notifications'>
+              <Link to='/Notifications/'>
+                <span className='main-menu-item-text'>
+                  <Icon type='bell' className='anticon-title' />
                   Notifications
                 </span>
               </Link>
