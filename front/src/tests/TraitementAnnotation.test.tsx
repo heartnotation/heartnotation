adef089e081af813f7966b4f2028d11c5cf56e9a
romain phet <romain.phet@gmail.com>
2019-02-08 19:50:08 +0100

    clean for merge

--- front/src/tests/TraitementAnnotation.test.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import Enzyme, { mount } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-
-Enzyme.configure({ adapter: new Adapter() });
-
-describe('Traitement annotation test', () => {});
