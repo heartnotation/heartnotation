beec395857a00ee565148d3774371f59a25d9402
Leclerc Marvin <marvin.leclerc31@gmail.com>
2019-02-08 12:09:12 +0100

    Change Tag structure to link with parent tags

--- back/src/restapi/tag/tagModel.go
+++ back/src/restapi/tag/tagModel.go
@@ -3,7 +3,8 @@ package tag
 // Tag database representation
 type Tag struct {
 	ID       uint   `gorm:"AUTO_INCREMENT" json:"id"`
-	ParentID uint   `json:"parent_id,omitempty"`
+	Parent   *Tag   `json:"parent,omitempty"`
+	ParentID *uint  `gorm:"TYPE:integer REFERENCES tag" json:"parent_id,integer,omitempty"`
 	Name     string `json:"name"`
 	Color    string `json:"color"`
 	IsActive bool   `json:"is_active"`
