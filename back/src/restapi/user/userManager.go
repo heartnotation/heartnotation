e478d07d04cef2f358f81429eed94b8c8d1f302b
Leclerc Marvin <marvin.leclerc31@gmail.com>
2019-02-09 15:43:05 +0100

    Change Profile struct & add CreateUser function
    ToDo : delete role_id in JSON sended by server

--- back/src/restapi/user/userManager.go
+++ back/src/restapi/user/userManager.go
@@ -1,10 +1,33 @@
 package user
 
 import (
+	"encoding/json"
 	"net/http"
 
 	u "restapi/utils"
 )
+
+// CreateUser function which receive a POST request and return a fresh-new user
+func CreateUser(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, http.StatusText(405), 405)
+		return
+	}
+
+	db := u.GetConnection()
+
+	user := Profile{}
+
+	json.NewDecoder(r.Body).Decode(&user)
+
+	err := db.Preload("Role").Create(&user).Error
+	if err != nil {
+		http.Error(w, err.Error(), 403)
+		return
+	}
+
+	u.Respond(w, user)
+}
 
 // GetAllUsers return users from database
 func GetAllUsers(w http.ResponseWriter, r *http.Request) {
