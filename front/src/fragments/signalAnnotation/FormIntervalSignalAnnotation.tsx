c22887a65c40f5e7fbe517e705b8236a9e685f7d
rolextaing <rolex.taing@gmail.com>
2019-02-14 10:16:35 +0100

    Avancement formulaire commment tag

--- front/src/fragments/signalAnnotation/FormIntervalSignalAnnotation.tsx
+++ front/src/fragments/signalAnnotation/FormIntervalSignalAnnotation.tsx
@@ -1,258 +1,80 @@
 import React, { Component } from 'react';
-import {
-    Form, Input, Tooltip, Icon, Cascader, Select, Row, Col, Checkbox, Button, AutoComplete,
-  } from 'antd';
-  import { FormComponentProps } from 'antd/lib/form';
-  import { RouteComponentProps, withRouter } from 'react-router';
-  const { Option } = Select;
-  const AutoCompleteOption = AutoComplete.Option;
-  
-  const residences = [{
-    value: 'zhejiang',
-    label: 'Zhejiang',
-    children: [{
-      value: 'hangzhou',
-      label: 'Hangzhou',
-      children: [{
-        value: 'xihu',
-        label: 'West Lake',
-      }],
-    }],
-  }, {
-    value: 'jiangsu',
-    label: 'Jiangsu',
-    children: [{
-      value: 'nanjing',
-      label: 'Nanjing',
-      children: [{
-        value: 'zhonghuamen',
-        label: 'Zhong Hua Men',
-      }],
-    }],
-  }];
+import { Form, Icon, Tabs, Select } from 'antd';
+import { FormComponentProps } from 'antd/lib/form';
+import { RouteComponentProps, withRouter } from 'react-router';
 
-  interface States {
+interface Props extends FormComponentProps, RouteComponentProps {}
 
+class FormIntervalSignalAnnotation extends Component<Props> {
+  public state = {
+    confirmDirty: false,
+    autoCompleteResult: []
+  };
+
+  public handleSubmit = (e: any) => {
+    e.preventDefault();
+    this.props.form.validateFieldsAndScroll((err, values) => {
+      if (!err) {
+        console.log('Received values of form: ', values);
+      }
+    });
   }
-  
-  interface Props extends FormComponentProps, RouteComponentProps {
+
+  public handleChange(value: any) {
+    console.log(`selected ${value}`);
   }
-  
-  class FormIntervalSignalAnnotation extends Component<Props, States>{
-    state = {
-      confirmDirty: false,
-      autoCompleteResult: [],
-    };
-  
-    handleSubmit = (e:any) => {
-      e.preventDefault();
-      this.props.form.validateFieldsAndScroll((err, values) => {
-        if (!err) {
-          console.log('Received values of form: ', values);
-        }
-      });
-    }
-  
-    handleConfirmBlur = (e:any) => {
-      const value = e.target.value;
-      this.setState({ confirmDirty: this.state.confirmDirty || !!value });
-    }
-  
-    compareToFirstPassword = (rule:any, value:any, callback:any) => {
-      const form = this.props.form;
-      if (value && value !== form.getFieldValue('password')) {
-        callback('Two passwords that you enter is inconsistent!');
-      } else {
-        callback();
-      }
-    }
-  
-    validateToNextPassword = (rule:any, value:any, callback:any) => {
-      const form = this.props.form;
-      if (value && this.state.confirmDirty) {
-        form.validateFields(['confirm'], { force: true });
-      }
-      callback();
-    }
-  
-    handleWebsiteChange = (value:any) => {
-      let autoCompleteResult:string[] = [];
-      if (!value) {
-        autoCompleteResult = [];
-      } else {
-        autoCompleteResult = ['.com', '.org', '.net'].map(domain => `${value}${domain}`);
-      }
-      this.setState({ autoCompleteResult });
-    }
-  
-    render() {
-      const { getFieldDecorator } = this.props.form;
-      const { autoCompleteResult } = this.state;
-  
-      const formItemLayout = {
-        labelCol: {
-          xs: { span: 24 },
-          sm: { span: 8 },
-        },
-        wrapperCol: {
-          xs: { span: 24 },
-          sm: { span: 16 },
-        },
-      };
-      const tailFormItemLayout = {
-        wrapperCol: {
-          xs: {
-            span: 24,
-            offset: 0,
-          },
-          sm: {
-            span: 16,
-            offset: 8,
-          },
-        },
-      };
-      const prefixSelector = getFieldDecorator('prefix', {
-        initialValue: '86',
-      })(
-        <Select style={{ width: 70 }}>
-          <Option value="86">+86</Option>
-          <Option value="87">+87</Option>
-        </Select>
+
+  public render() {
+    const TabPane = Tabs.TabPane;
+    const Option = Select.Option;
+    const children = [];
+    for (let i = 10; i < 36; i++) {
+      children.push(
+        <Option key={i.toString(36) + i}>{i.toString(36) + i}</Option>
       );
-  
-      const websiteOptions = autoCompleteResult.map(website => (
-        <AutoCompleteOption key={website}>{website}</AutoCompleteOption>
-      ));
-  
-      return (
-        <Form onSubmit={this.handleSubmit}>
-          <Form.Item
-            {...formItemLayout}
-            label="E-mail"
-          >
-            {getFieldDecorator('email', {
-              rules: [{
-                type: 'email', message: 'The input is not valid E-mail!',
-              }, {
-                required: true, message: 'Please input your E-mail!',
-              }],
-            })(
-              <Input />
-            )}
-          </Form.Item>
-          <Form.Item
-            {...formItemLayout}
-            label="Password"
-          >
-            {getFieldDecorator('password', {
-              rules: [{
-                required: true, message: 'Please input your password!',
-              }, {
-                validator: this.validateToNextPassword,
-              }],
-            })(
-              <Input type="password" />
-            )}
-          </Form.Item>
-          <Form.Item
-            {...formItemLayout}
-            label="Confirm Password"
-          >
-            {getFieldDecorator('confirm', {
-              rules: [{
-                required: true, message: 'Please confirm your password!',
-              }, {
-                validator: this.compareToFirstPassword,
-              }],
-            })(
-              <Input type="password" onBlur={this.handleConfirmBlur} />
-            )}
-          </Form.Item>
-          <Form.Item
-            {...formItemLayout}
-            label={(
+    }
+
+    return (
+      <div>
+        {' '}
+        <Tabs defaultActiveKey='2'>
+          <TabPane
+            tab={
               <span>
-                Nickname&nbsp;
-                <Tooltip title="What do you want others to call you?">
-                  <Icon type="question-circle-o" />
-                </Tooltip>
+                <Icon type='tags' />
+                Tags
               </span>
-            )}
+            }
+            key='1'
           >
-            {getFieldDecorator('nickname', {
-              rules: [{ required: true, message: 'Please input your nickname!', whitespace: true }],
-            })(
-              <Input />
-            )}
-          </Form.Item>
-          <Form.Item
-            {...formItemLayout}
-            label="Habitual Residence"
+            <Select
+              mode='multiple'
+              style={{ width: '100%' }}
+              placeholder='Please select'
+              defaultValue={['a10', 'c12']}
+              onChange={this.handleChange}
+            >
+              {children}
+            </Select>
+          </TabPane>
+          <TabPane
+            tab={
+              <span>
+                <Icon type='form' />
+                Comments
+              </span>
+            }
+            key='2'
           >
-            {getFieldDecorator('residence', {
-              initialValue: ['zhejiang', 'hangzhou', 'xihu'],
-              rules: [{ type: 'array', required: true, message: 'Please select your habitual residence!' }],
-            })(
-              <Cascader options={residences} />
-            )}
-          </Form.Item>
-          <Form.Item
-            {...formItemLayout}
-            label="Phone Number"
-          >
-            {getFieldDecorator('phone', {
-              rules: [{ required: true, message: 'Please input your phone number!' }],
-            })(
-              <Input addonBefore={prefixSelector} style={{ width: '100%' }} />
-            )}
-          </Form.Item>
-          <Form.Item
-            {...formItemLayout}
-            label="Website"
-          >
-            {getFieldDecorator('website', {
-              rules: [{ required: true, message: 'Please input website!' }],
-            })(
-              <AutoComplete
-                dataSource={websiteOptions}
-                onChange={this.handleWebsiteChange}
-                placeholder="website"
-              >
-                <Input />
-              </AutoComplete>
-            )}
-          </Form.Item>
-          <Form.Item
-            {...formItemLayout}
-            label="Captcha"
-            extra="We must make sure that your are a human."
-          >
-            <Row gutter={8}>
-              <Col span={12}>
-                {getFieldDecorator('captcha', {
-                  rules: [{ required: true, message: 'Please input the captcha you got!' }],
-                })(
-                  <Input />
-                )}
-              </Col>
-              <Col span={12}>
-                <Button>Get captcha</Button>
-              </Col>
-            </Row>
-          </Form.Item>
-          <Form.Item {...tailFormItemLayout}>
-            {getFieldDecorator('agreement', {
-              valuePropName: 'checked',
-            })(
-              <Checkbox>I have read the <a href="">agreement</a></Checkbox>
-            )}
-          </Form.Item>
-          <Form.Item {...tailFormItemLayout}>
-            <Button type="primary" htmlType="submit">Register</Button>
-          </Form.Item>
-        </Form>
-      );
-    }
+            ermHazerDDeEee
+          </TabPane>
+        </Tabs>
+      </div>
+    );
+    return <Form onSubmit={this.handleSubmit} />;
   }
+}
 
-  export default Form.create({ name: 'register' })(withRouter(FormIntervalSignalAnnotation));
+export default Form.create({ name: 'register' })(
+  withRouter(FormIntervalSignalAnnotation)
+);
