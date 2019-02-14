0a5d9cef838863a816f78ee1868b9b27fc90de52
Théo Holander <holandertheo@gmail.com>
2019-02-14 09:28:12 +0100

    commit before changing branch

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './assets/styles/App.css';
-import AppRouter, { AppRoute } from './Routes';
+import AppRouter from './Routes';
 import AnnotationForm from './pages/CreateAnnotationForm';
 import UserCreation from './pages/UserCreation';
 import TagCreation from './pages/TagCreation';
@@ -10,101 +10,95 @@ import Dashboard from './pages/Dashboard';
 import SignalAnnotation from './pages/SignalAnnotation';
 import { api } from './utils';
 
-interface State {
-  defaultRoute: AppRoute;
-  routes: AppRoute[];
-  hiddenRoutes: AppRoute[];
-}
+const r = {
+  defaultRoute: {
+    path: '/',
+    exact: true,
+    component: () => <Dashboard getAnnotations={api.getAnnotations} />,
+    title: 'Dashboard'
+  },
+  hiddenRoutes: [
+    {
+      path: '/annotations/:id',
+      component: () => (
+        <SignalAnnotation
+          getAnnotation={api.getAnnotationById}
+          changeAnnotation={api.changeAnnotation}
+        />
+      ),
+      title: 'Signal annotation'
+    }
+  ],
+  routes: [
+    {
+      path: '/new/annotations',
+      component: () => (
+        <AnnotationForm
+          getTags={api.getTags}
+          getOrganizations={api.getOrganizations}
+          getAnnotations={api.getAnnotations}
+          checkSignal={api.checkSignal}
+          sendAnnotation={api.sendAnnotation}
+        />
+      ),
+      title: 'Create annotation',
+      iconName: 'plus'
+    },
+    {
+      path: '/new/users',
+      component: () => (
+        <UserCreation
+          getOrganizations={api.getOrganizations}
+          getRoles={api.getRoles}
+          sendUser={api.sendUser}
+        />
+      ),
+      title: 'Create User',
+      iconName: 'user-add'
+    },
+    {
+      path: '/about',
+      component: () => <h2>About</h2>,
+      title: 'About',
+      iconName: 'question'
+    },
+    {
+      path: '/users',
+      component: Users,
+      exact: true,
+      title: 'Users',
+      iconName: 'user'
+    },
+    {
+      path: '/new/tags',
+      component: TagCreation,
+      title: 'Create Tags',
+      iconName: 'tag'
+    },
+    {
+      path: '/tags',
+      exact: true,
+      component: Tags,
+      title: 'Tags',
+      iconName: 'tags'
+    }
+  ]
+};
 
-class App extends Component<any, State> {
+export default class App extends Component {
   constructor(props: any) {
     super(props);
-    /**
-     * TODO remplacer pour récupérer les routes en fonctions du rôle de l'utilisateur connecté.
-     */
-    const defaultRoute = {
-      path: '/',
-      exact: true,
-      component: () => <Dashboard getAnnotations={api.getAnnotations} />,
-      title: 'Dashboard'
-    };
     this.state = {
-      defaultRoute,
-      hiddenRoutes: [
-        {
-          path: '/annotations/:id',
-          component: () => (
-            <SignalAnnotation
-              getAnnotation={api.getAnnotationById}
-              changeAnnotation={api.changeAnnotation}
-            />
-          ),
-          title: 'Signal annotation'
-        }
-      ],
-      routes: [
-        {
-          path: '/new/annotations',
-          component: () => (
-            <AnnotationForm
-              getTags={api.getTags}
-              getOrganizations={api.getOrganizations}
-              getAnnotations={api.getAnnotations}
-              checkSignal={api.checkSignal}
-              sendAnnotation={api.sendAnnotation}
-            />
-          ),
-          title: 'Create annotation',
-          iconName: 'plus'
-        },
-        {
-          path: '/new/users',
-          component: UserCreation,
-          title: 'Create User',
-          iconName: 'user-add'
-        },
-        {
-          path: '/about',
-          component: () => <h2>About</h2>,
-          title: 'About',
-          iconName: 'question'
-        },
-        {
-          path: '/users',
-          component: Users,
-          exact: true,
-          title: 'Users',
-          iconName: 'user'
-        },
-        {
-          path: '/new/tags',
-          component: TagCreation,
-          title: 'Create Tags',
-          iconName: 'tag'
-        },
-        {
-          path: '/tags',
-          exact: true,
-          component: Tags,
-          title: 'Tags',
-          iconName: 'tags'
-        }
-      ]
+      user: undefined
     };
   }
-
-  public render() {
-    const { defaultRoute, routes, hiddenRoutes } = this.state;
+  public render = () => {
     return (
-      <div>
-        <AppRouter
-          defaultRoute={defaultRoute}
-          routes={routes}
-          hiddenRoutes={hiddenRoutes}
-        />
-      </div>
+      <AppRouter
+        defaultRoute={r.defaultRoute}
+        routes={r.routes}
+        hiddenRoutes={r.hiddenRoutes}
+      />
     );
   }
 }
-
-export default App;
