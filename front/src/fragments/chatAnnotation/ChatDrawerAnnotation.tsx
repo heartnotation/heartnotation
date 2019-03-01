1a41055ddcc206012dfacb6a00ea9dabfa9c48f8
Théo Holander <holandertheo@gmail.com>
2019-03-01 13:36:46 +0100

    made comment drawer stateless

--- front/src/fragments/chatAnnotation/ChatDrawerAnnotation.tsx
+++ front/src/fragments/chatAnnotation/ChatDrawerAnnotation.tsx
@@ -1,57 +1,35 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import { Drawer, Button } from 'antd';
 import CommentChatAnnotation from '../chatAnnotation/CommentChatAnnotation';
 
-interface State {
-  visible: boolean;
-}
 interface Props {
   annotation_id: number;
 }
-class ChatDrawerAnnotation extends Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-  }
 
-  public state = { visible: false };
-
-  public showDrawer = () => {
-    this.setState({
-      visible: true
-    });
-  }
-
-  public onClose = () => {
-    this.setState({
-      visible: false
-    });
-  }
-
-  public render() {
-    return (
-      <div className='chat-drawer-container'>
-        <Button
-          type='primary'
-          icon='message'
-          size='large'
-          onClick={this.showDrawer}
-          className='btn-space'
-        >
-          Comments
-        </Button>
-        <Drawer
-          title='Annotation Comments'
-          placement='right'
-          width='512'
-          closable={false}
-          onClose={this.onClose}
-          visible={this.state.visible}
-        >
-          <CommentChatAnnotation annotation_id={this.props.annotation_id} />
-        </Drawer>
-      </div>
-    );
-  }
-}
-
-export default ChatDrawerAnnotation;
+export default (props: Props) => {
+  const { annotation_id } = props;
+  const [visible, setVisible] = useState(false);
+  return (
+    <div className='chat-drawer-container'>
+      <Button
+        type='primary'
+        icon='message'
+        size='large'
+        onClick={() => setVisible(true)}
+        className='btn-space'
+      >
+        Comments
+      </Button>
+      <Drawer
+        title='Annotation Comments'
+        placement='right'
+        width='512'
+        closable={false}
+        onClose={() => setVisible(false)}
+        visible={visible}
+      >
+        <CommentChatAnnotation annotation_id={annotation_id} />
+      </Drawer>
+    </div>
+  );
+};
