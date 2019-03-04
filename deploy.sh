5dee4fd47d5b2002a985e6fe6d8d6061ed7694a9
Théo Holander <holandertheo@gmail.com>
2019-03-04 16:10:34 +0100

    Delete deploy.sh

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo nJyG#cg2hY5BoB | docker login -u heartnotation --password-stdin
-docker build --build-arg api=http://heartnotation.local:8080 --build-arg client_id=172888593948-qm2mn5jrrint8saav1hndus58583ll34.apps.googleusercontent.com -t heartnotation/web-app:latest ./front
-docker build -t heartnotation/rest-api:latest ./back
-docker build -t heartnotation/database:latest ./db
-
-docker push heartnotation/rest-api:latest
-docker push heartnotation/web-app:latest
-docker push heartnotation/database:latest
